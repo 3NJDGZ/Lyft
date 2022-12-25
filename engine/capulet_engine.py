6c0685f74333bcff854dd0a2b481d172348890fd
Alejandro.DGZ <87236751+3NJDGZ@users.noreply.github.com>
2022-12-25 16:26:01 +0000

    Second Commit

---
+++ engine/capulet_engine.py
@@ -0,0 +1,9 @@
+from engine.engine import Engine
+
+class CapuletEngine(Engine):
+    def __init__(self, last_service_mileage: int, current_mileage: int):
+        self.__last_service_mileage = last_service_mileage
+        self.__current_mileage = current_mileage
+
+    def needs_service(self):
+        return self.__current_mileage - self.__last_service_mileage > 30000
